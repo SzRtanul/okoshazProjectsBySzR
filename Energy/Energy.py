0c51df6cb3e82b543e4384fe99871f0e65e9e9ce
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-05-22 11:01:12 +0200

    Előkészítés

---
+++ Energy/Energy.py
@@ -0,0 +1,12 @@
+class Energy:
+     def __init__(self):
+          pass
+     
+     def getUsedEnergy(self):
+          pass
+     
+     def getInEnergy(self):
+          pass
+     
+     def storedEnergy(self):
+          pass
